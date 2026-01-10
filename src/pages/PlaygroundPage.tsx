302b1363fb9e16cb6e1fbaa56e9e9e8f68a9c34d
shuvam_776 <shuvamsatapathi@gmail.com>
2026-01-10 17:57:11 +0530

    Footer section

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,11 +1,9 @@
-import Footer from '@/components/footer/Footer'
 import PlaygroundContent from '@/components/ui/playground'
 
 export default function Playground() {
   return (
     <main>
       <PlaygroundContent />
-      <Footer />
     </main>
   )
 }
